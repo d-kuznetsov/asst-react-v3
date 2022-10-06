f50743b0476228a79ea6e61f711a7ccb42420f03
Dmitry Kuznetsov <dmitry.kuznetsov.it@proton.me>
2022-10-06 02:04:49 +0200

    make simple config for testing

---
+++ src/config.js
@@ -0,0 +1,36 @@
+export default {
+  steps: [
+    {
+      id: "S1",
+      title: "First step",
+      fields: [
+        {
+          id: "F1A",
+          title: "A field",
+          type: "CA",
+        },
+        {
+          id: "F1B",
+          title: "B field",
+          type: "CB",
+        },
+      ],
+    },
+    {
+      id: "S2",
+      title: "Second step",
+      fields: [
+        {
+          id: "F1C",
+          title: "C field",
+          type: "CA",
+        },
+        {
+          id: "F1D",
+          title: "D field",
+          type: "CB",
+        },
+      ],
+    },
+  ],
+};
